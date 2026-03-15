d7a9f6492312e3e3321f0c136ed0f1e5bbadb419
compotapp <alexandrpotapov90@gmail.com>
2026-03-15 14:04:06 +0300

    feature-0001: first-commit

---
+++ production-of-numbers/src/main/java/com/pot/app/productionofnumbers/ProductionOfNumbersApplication.java
@@ -0,0 +1,13 @@
+package com.pot.app.productionofnumbers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionOfNumbersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductionOfNumbersApplication.class, args);
+    }
+
+}
